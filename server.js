6d1dfb3037f40dc24dc3ea6ea8e9c22bc2529650
Albert "Christian" DeMarco <Kelter-1313>
2022-04-10 01:42:12 -0400

    fixed_variable

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get("/app", (req, res) => {
   res.type("text/plain")
 })
 
-app.get("/app/flip", (req,res) => {
+app.get("/app/flip/", (req,res) => {
   res.status(200).json({"flip":coinFlip()})
 })
 
@@ -95,16 +95,16 @@ app.get("/app/echo/:number", (req, res) => {
   res.status(200).json({"message": req.params.number})
 })
 
-app.get("/app/flips", (req,res) =>{
+app.get("/app/flips/:number/", (req,res) =>{
   var a = coinFlips(req.params.number)
   res.status(200).json({"raw":a, "summary":countFlips(a)})
 })
 
-app.get("/app/flip/call/heads", (req,res) =>{
+app.get("/app/flip/call/heads/", (req,res) =>{
   res.status(200).json({"flips":flipAHead(req.params.number)})
 })
 
-app.get("/app/flip/call/tails", (req,res) =>{
+app.get("/app/flip/call/tails/", (req,res) =>{
   res.status(200).json({"flips":flipATail(req.params.number)})
 })
 
