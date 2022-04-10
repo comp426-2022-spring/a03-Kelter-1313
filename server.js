340f5c1c35d7272592cfb4a08ff91b8f70c50547
Albert "Christian" DeMarco <Kelter-1313>
2022-04-10 00:58:28 -0400

    Alterations to Server

--- server.js
+++ server.js
@@ -18,37 +18,12 @@ const port = args.port || process.env.PORT || 3000
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./www/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-fs.readFile("./www/index.html", 'utf8' , (err, data) => {
-    if (err) {
-      console.error(err)
-      return
-//      process.exit(1)
-    }
-    console.log(data)
-// If there is an error, put it on the console error, return, and exit with error code 1. 
-// Do not be nice about exiting.
-
-// Define a const `server` as an arrow function using http.createServer. 
-// Use the documentation for the node.js http module. 
-// The function should have three responses: 
-// 1. status code 200, 
-// 2. set a header with content type `text/html`, and 
-// 3. end with the data that you are reading in from ./www/index.html.
-
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html')
-  res.end(data)
-})
-
-
-// Start the `server` const listening on the port defined by argument in your `port` const. 
-// Put the exact message `Server listening on port ${port}` on the console log. 
-
 server.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
+  console.log(`App is running on port ${port}`)
 })
 
+app.use(function(req,res) {
+  res.status(404).end("Endpoint does not exist")
+  res.type("text/plain")
+
 })
-// That's it! You're all done!
